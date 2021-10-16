bc21a243cf0c72f73f65c7c4be394ca0dac20473
Harrsh Patel <me.harrsh.patel@gmail.com>
2021-10-16 19:06:42 +0530

    Hamburger menu icon

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!!");
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+
+hamburgerBtn.addEventListener("click", function () {
+    if (hamburgerBtn.classList.contains("open")) {
+        hamburgerBtn.classList.remove("open");
+    } else {
+        hamburgerBtn.classList.add("open");
+    }
+});
